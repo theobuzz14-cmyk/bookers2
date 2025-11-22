d1f390ce47ac680d282db1d646c98ff4e7787f87
theobuzz14-cmyk <240716808+theobuzz14-cmyk@users.noreply.github.com>
2025-11-22 09:27:13 +0000

    [finish]books,usersコントローラの作成、投稿フォーム作成

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,26 @@
+class BooksController < ApplicationController
+  
+  def create
+    @book = Book.new
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_parh
+  end
+
+  def index
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def show
+    book = Book.find(params[:id])
+  end
+
+
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body, :image)
+  end
+
+end
