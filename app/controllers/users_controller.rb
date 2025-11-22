ce3b38a0c853ce9d26d14146bd2a6d7e6c9e2c0b
theobuzz14-cmyk <240716808+theobuzz14-cmyk@users.noreply.github.com>
2025-11-22 19:09:58 +0000

    [update]アソシエーション記述、userコントローラ記述←エラー検証中

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,11 +2,22 @@ class UsersController < ApplicationController
 
   def index
     @book = Book.new
+    @user = User.find(params[:id])
+    @books = @user.post_images
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+  end
+
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
   end
 end
