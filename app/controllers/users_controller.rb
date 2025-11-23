44d3d1a3e1a8d262ddc93f49d2bc620b8c9cd3a8
theobuzz14-cmyk <240716808+theobuzz14-cmyk@users.noreply.github.com>
2025-11-23 14:16:52 +0000

    [finish]users show作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def show
     @book = Book.find(params[:id])
-    @user = User.find(current_user.id)
+    @user = User.find(params[:id])
     @books = @user.books
   end
 
