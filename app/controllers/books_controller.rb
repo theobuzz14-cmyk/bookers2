39be6ce2876aa4f8fdce311c85086b4822d655c4
theobuzz14-cmyk <240716808+theobuzz14-cmyk@users.noreply.github.com>
2025-11-24 09:45:45 +0000

    [update]booksコントローラshowアクション訂正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -20,7 +20,7 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
-    @user = User.find(current_user.id)
+    @user = User.find(@book.user_id)
     @books = @user.books
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,4 @@ Rails.application.routes.draw do
   get 'about' => 'homes#about'
   resources :books , only: [:index, :show, :destroy, :create, :edit, :update]
   resources :users , only: [:show, :edit, :index, :update]
-  resources :homes , only: [:index]
-
 end
